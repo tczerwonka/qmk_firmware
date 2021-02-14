e42fc409e89d20014b2a7d46b95307e1c158f192
T Czerwonka <tczerwonka@gmail.com>
2021-02-13 19:16:47 -0600

    three working layers

--- keyboards/1upkeyboards/sweet16/keymaps/tczerwonka/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/tczerwonka/keymap.c
@@ -4,8 +4,9 @@
 
 #include QMK_KEYBOARD_H
 
-#define _BASE 0
-#define _SECOND 1
+#define _BASE 	0
+#define _OTHER 	1
+#define _V6 	2
 
 #define PLEX_LED_0	D7
 #define PLEX_LED_1	E6
@@ -15,21 +16,36 @@
 enum custom_keycodes {
   FFS = SAFE_RANGE,
   COMMIT_CHECK,
-  SHOW_COMPARE
+  SHOW_COMPARE,
+  GIT_DIFF,
+  GIT_ADD,
+  GIT_PUSH,
+  GIT_COMMIT,
+  COLON_COLON,
+  ONETWENTYSEVEN,
+  OHFFS
 };
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_4x4(
-        KC_7, KC_8,   KC_9,   KC_ASTR,
+        KC_7, KC_8,   KC_9,   TO(_OTHER),
         KC_4, KC_5,   KC_6,   KC_SLSH,
-        KC_1, KC_2,   KC_3,   KC_MINS,
-        KC_0, KC_ENT, KC_DOT, MO(_SECOND)
+        KC_1, KC_2,   KC_3,   KC_PLUS,
+        KC_0, KC_DOT, KC_MINS, KC_ENT
     ),
-    [_SECOND] = LAYOUT_ortho_4x4(
-        KC_7, KC_8,   KC_9,   KC_ASTR,
-        KC_4, KC_5,   KC_6,   KC_SLSH,
-        KC_1, KC_2,   KC_3,   KC_MINS,
-        FFS, COMMIT_CHECK, SHOW_COMPARE, KC_EQL
+    [_OTHER] = LAYOUT_ortho_4x4(
+        FFS, 		OHFFS,   	KC_NO,   	TO(_V6),
+        A(KC_TAB), 	KC_VOLD,   	KC_VOLU,   	KC_MUTE,
+        GIT_DIFF, 	GIT_ADD,   	GIT_COMMIT,   	GIT_PUSH,
+        KC_SPACE, 	COMMIT_CHECK, 	SHOW_COMPARE, 	KC_ENT
+    ),
+    [_V6] = LAYOUT_ortho_4x4(
+        KC_7, 	KC_8,   	KC_9,		TO(_BASE),
+        KC_4, 	KC_5,   	KC_6,		ONETWENTYSEVEN,
+        KC_1, 	KC_2,   	KC_3,		KC_SLSH,
+        KC_0, 	S(KC_SCOLON), 	COLON_COLON,	KC_ENT
     )
 };
 
@@ -38,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Layer LEDs act as binary indication of current layer
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    case _BASE:
+    case _OTHER:
 	writePinLow(PLEX_LED_0);
 	writePinHigh(PLEX_LED_2);
         break;
-    case _SECOND:
+    case _V6:
 	writePinLow(PLEX_LED_2);
 	writePinHigh(PLEX_LED_0);
         break;
@@ -95,10 +111,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case FFS:
             if (record->event.pressed) {
-                SEND_STRING("oh ffs");
+                SEND_STRING("ffs ");
             }
             return false;
             break;
+        case OHFFS:
+            if (record->event.pressed) {
+                SEND_STRING("oh ffs ");
+            }
+            return false;
+            break;
+
         case COMMIT_CHECK:
             if (record->event.pressed) {
                 SEND_STRING("commit check");
@@ -108,6 +131,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SHOW_COMPARE:
             if (record->event.pressed) {
                 SEND_STRING("show | compare\n");
+            }
+            return false;
+            break;
+
+        case COLON_COLON:
+            if (record->event.pressed) {
+                SEND_STRING("::");
+            }
+            return false;
+            break;
+        case ONETWENTYSEVEN:
+            if (record->event.pressed) {
+                SEND_STRING("/127");
+            }
+            return false;
+            break;
+
+        case GIT_COMMIT:
+            if (record->event.pressed) {
+                SEND_STRING("git commit -m \"");
+            }
+            return false;
+            break;
+        case GIT_PUSH:
+            if (record->event.pressed) {
+                SEND_STRING("git push\n");
+            }
+            return false;
+            break;
+        case GIT_ADD:
+            if (record->event.pressed) {
+                SEND_STRING("git add ");
+            }
+            return false;
+            break;
+        case GIT_DIFF:
+            if (record->event.pressed) {
+                SEND_STRING("git diff\n");
             }
             return false;
             break;
